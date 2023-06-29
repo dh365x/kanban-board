383e7cb123f942dea0a879e53d072f304af504cf
dh365x <dh365x@gmail.com>
2023-06-29 22:57:15 +0900

    드롭 & 드래그

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function App() {
-	return <></>;
+	const onDragEnd = () => {};
+
+	return (
+		<DragDropContext onDragEnd={onDragEnd}>
+			<div>
+				<Droppable droppableId="list">
+					{(provied) => (
+						<ul ref={provied.innerRef} {...provied.droppableProps}>
+							<Draggable draggableId="item1" index={0}>
+								{(provied) => (
+									<li
+										ref={provied.innerRef}
+										{...provied.draggableProps}
+										{...provied.dragHandleProps}
+									>
+										item①
+									</li>
+								)}
+							</Draggable>
+							<Draggable draggableId="item2" index={1}>
+								{(provied) => (
+									<li
+										ref={provied.innerRef}
+										{...provied.draggableProps}
+										{...provied.dragHandleProps}
+									>
+										item②
+									</li>
+								)}
+							</Draggable>
+							{provied.placeholder}
+						</ul>
+					)}
+				</Droppable>
+			</div>
+		</DragDropContext>
+	);
 }
 
 export default App;
